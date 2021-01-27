a1a3debcbd566a70810bc14fa169d6ae15c13f22
Maxi <61260740+macencic98@users.noreply.github.com>
2021-01-27 13:32:25 -0300

    Update mostrarPersonas.js

--- Ejercicio4/mostrarPersonas.js
+++ Ejercicio4/mostrarPersonas.js
@@ -5,7 +5,7 @@ const reqprom = require('request-promise');
 const urlsearch = 'https://reclutamiento-14cf7.firebaseio.com/personas.json';
 //creo una variable donde inserto la URL a ingresar
 
-//a la variable que supone la petición(tambien conocida como options), le indico el verbo HTTP que indica
+//a la variable que supone la petición(tambien conocida como options e indican las caracteristicas de la peticion), le indico el verbo HTTP que indica
 //el nombre de la petición que se le va a hacer al servidor, también le indico la URL
 //a la cual le quiero hacer esa petición, y establezco json en true para que devuelva los datos
 //en ese formato
